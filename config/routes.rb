0672b48c48c9441698a7c93bc20da13bb67832bf
Nicolas Rey <nic.m.rey@gmail.com>
2022-12-28 15:33:14 -0800

    Added home page, linked to and styled signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   devise_scope :user do
-    root to: "devise/sessions#new"
+  # root to: "devise/sessions#new"
   end
 
   resources :rooms
