9d0ff1cb543780754310bfba7f46324c22816f88
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-06 16:07:29 -0700

    Fixed bug where you couldnt delete a user unless messages and rooms were deleted first.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :account
+  has_one :account, dependent: :destroy
 
   after_create :create_account
 
