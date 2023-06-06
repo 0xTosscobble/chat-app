0c2a9516267f93227bd7e231d4d4c08c2ba466c9
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-06 16:06:18 -0700

    Add accounts foreign key.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_190839) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_224547) do
   create_table "accounts", force: :cascade do |t|
     t.string "username"
     t.text "image_data"
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_190839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
 end
