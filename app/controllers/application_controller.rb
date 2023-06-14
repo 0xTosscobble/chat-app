82a3d9d35752353ad6f2d4f18e3123c7181f788f
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-14 15:59:18 -0700

    new resource path and cleaning up rooms page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
+    def after_sign_up_path_for(resource)
+        list_rooms_path
+    end
+
     def after_sign_in_path_for(resource)
-        home_index_path
+        list_rooms_path
     end
 
       
