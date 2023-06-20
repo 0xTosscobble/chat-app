14bc8bcb1d4fe922c83912713f25cadcc9936cc5
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-20 10:11:06 -0700

    Fixed account bug

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -60,7 +60,7 @@ class AccountsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_account
-      @account = Account.find(params[:id])
+      @account = Account.find(current_user[:id])
     end
 
     # Only allow a list of trusted parameters through.
